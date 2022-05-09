199ea3cfaabf616a76905f31c52fc31354a02292
AlexGolubnichy <alexandergolubnichy@yandex.ru>
2022-05-10 01:27:59 +0300

    edit counter

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,5 +1,11 @@
 let successShot = 0;
 let bossShot = 0;
+clearCounter = () => {
+    successShot = 0;
+    bossShot = 0;
+    document.getElementById('dead').textContent = 0;
+    document.getElementById('lost').textContent = 0;
+}
 
 for (let i = 1; i < 10; i++) {
     let targetHole = document.getElementById('hole' + i);
@@ -9,14 +15,14 @@ for (let i = 1; i < 10; i++) {
             document.getElementById('dead').textContent = successShot;
             if (successShot === 10) {
                 alert('Вы победили!');
-                return successShot = 0, bossShot = 0;
+               clearCounter();
             }
         } else {
             bossShot += 1;
             document.getElementById('lost').textContent = bossShot;
             if (bossShot === 5) {
                 alert('Поражение((');
-                return successShot = 0, bossShot = 0;
+              clearCounter();
             }
         }
     }  
