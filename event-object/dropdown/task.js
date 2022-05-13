235780bba9c9482d7175506d973cb6951c9d9d6f
AlexGolubnichy <alexandergolubnichy@yandex.ru>
2022-05-13 18:34:19 +0300

    dropdown complete

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
+
+function toggleClassList() {
+	dropdownList.classList.toggle("dropdown__list_active");
+};
+
+dropdownValue.addEventListener('click', toggleClassList);
+
+function changeValue(event) {
+	event.preventDefault();
+	let options = event.options;
+	dropdownList.classList.remove("dropdown__list_active");
+	dropdownValue.textContent = options.textContent;	
+};
+
+dropdownItems.forEach(elem => {
+	elem.addEventListener('click', changeValue);
+});
