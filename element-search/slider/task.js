45ace96cf03dade44e0041bed44bfc1881a8e641
AlexGolubnichy <alexandergolubnichy@yandex.ru>
2022-05-14 00:31:11 +0300

    edit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,30 @@
-const imageElms = document.querySelectorAll(".slider__item");
-const sliderCount = imageElms.length;
-const dotElms = document.querySelectorAll(".slider__dot");
+const imageElms = Array.from(document.querySelectorAll(".slider__item"));
 const leftBtn = document.querySelector(".slider__arrow_prev");
 const rightBtn = document.querySelector(".slider__arrow_next");
 
-let activeSlideIndex = 0;
 
-function toggleActiveSlide(activeSlideIndex) {
-    let activeSlide = document.querySelector(".slider__item_active");
-    let activeDot = document.querySelector(".slider__dot_active");
-    activeSlide.classList.remove('slider__item_active');
-    activeDot.classList.remove('slider__dot_active');
-    imageElms.item(activeSlideIndex).classList.add('slider__item_active');
-    dotElms.item(activeSlideIndex).classList.add('slider__dot_active');   
+let activeSlideIndex = imageElms.findIndex((item) => item.classList.contains('slider__item_active'));
+
+
+function slider(newSlideActive) {
+    imageElms[activeSlideIndex].classList.remove('slider__item_active');
+    imageElms[newSlideActive].classList.add('slider__item_active');
+    activeSlideIndex = newSlideActive;
 }
 
-leftBtn.addEventListener('click', function(){
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-        activeSlideIndex = sliderCount - 1;
-    }
-    toggleActiveSlide(activeSlideIndex);
-    return activeSlideIndex;
-}); 
-
-rightBtn.addEventListener('click', function(){
-    activeSlideIndex++;
-    if (activeSlideIndex === sliderCount) {
-        activeSlideIndex = 0;
-    }
-    toggleActiveSlide(activeSlideIndex);
-    return activeSlideIndex;
+rightBtn.addEventListener('click', () => {
+    let newSlide = activeSlideIndex + 1;
+    newSlide = newSlide >= imageElms.length ? 0 : newSlide;
+    slider(newSlide);
 });
 
-for (let k = 0; k < sliderCount; k++) {
-    dotElms.item(k).addEventListener('mouseover', () => toggleActiveSlide(k));
-}
+leftBtn.addEventListener('click', () => {
+    let newSlide = activeSlideIndex - 1;
+    newSlide = newSlide < 0 ? imageElms.length - 1 : newSlide;
+    slider(newSlide);
+}); 
+
+
+
+
+
